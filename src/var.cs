423bb012025f1267c43c52c5145453e71a981586
Unknown6656 <unknown84@gmx.de>
2024-02-12 14:14:49 +0100

    added var drop-in class

---
+++ src/var.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.Diagnostics;
+using System;
+
+
+#if NET50_OR_GREATER
+    #pragma warning disable CA1050 // Declare types in namespaces
+
+    #if NET6_0_OR_GREATER
+        #pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+    #endif
+#else
+    #pragma warning disable IDE0079
+#endif
+
+#pragma warning disable IDE1006 // Naming Styles
+
+
+[Obsolete("Do not use 'var' as an type identifier. Specify the full type identifier instead.", error: true)]
+[EditorBrowsable(EditorBrowsableState.Never), CompilerGenerated, DebuggerNonUserCode, DebuggerStepThrough]
+public ref struct var;
+
+#pragma warning restore
