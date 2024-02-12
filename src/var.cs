6b012dd6968095c30e4e5e6a5f5c69990863d775
Unknown6656 <unknown84@gmx.de>
2024-02-12 14:44:42 +0100

    prevented instantiation of 'var' (even through reflection).

--- src/var.cs
+++ src/var.cs
@@ -17,8 +17,28 @@ using System;
 #pragma warning disable IDE1006 // Naming Styles
 
 
-[Obsolete("Do not use 'var' as an type identifier. Specify the full type identifier instead.", error: true)]
+[Obsolete(MESSAGE, error: true)]
 [EditorBrowsable(EditorBrowsableState.Never), CompilerGenerated, DebuggerNonUserCode, DebuggerStepThrough]
-public ref struct var;
+public sealed class var
+    : Exception
+{
+    private const string MESSAGE = "Do not use 'var' as an type identifier. Specify the full type identifier instead.";
+    private const int HRESULT = unchecked((int)0x8013153A); // COR_E_INVALIDPROGRAM
+
+
+    /// <summary>
+    /// Prevent instantiation of this class (even through reflection).
+    /// <para/>
+    /// As we cannot use <see href="https://learn.microsoft.com/en-us/dotnet/api/system.invalidprogramexception"/> in <c>NETSTANDARD 1.0</c>, we have to provide the <see cref="Exception.HResult"/> manually.
+    /// </summary>
+    private var()
+        : base(MESSAGE)
+    {
+        HResult = HRESULT;
+        HelpLink = "https://docs.microsoft.com/dotnet/csharp/language-reference/keywords/var";
+
+        throw this;
+    }
+}
 
 #pragma warning restore
